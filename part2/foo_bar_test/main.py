fcd12d27fd546365e1f2a72c85ec4ba9a29b83ea
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-08-11 23:07:28 +0500

    тест 22

--- part2/foo_bar_test/main.py
+++ part2/foo_bar_test/main.py
@@ -1,8 +1,13 @@
 # Вам дана обычная функция foo, перепишите ее на лямбду функцию.
 
-def foo(n):
-    for i in range(n):
-        yield i**2
+# #def foo(n):
+#     for i in range(n):
+#         yield i**2
+
+
+new_foo = lambda x: (i**2 for i in range(x))
 
 if __name__ == "__main__":
-    print([x for x in foo(5)])
+    #  print([x for x in new_foo(5)])
+    print(list(new_foo(5)))
+

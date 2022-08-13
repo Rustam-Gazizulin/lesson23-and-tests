74e93460f284a46c4d4332d0b096060339aaf4b6
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-08-13 12:40:21 +0500

    тест 28

--- part2/top_3/main.py
+++ part2/top_3/main.py
@@ -2,11 +2,12 @@
 # которая на вход принимает строку и 
 # возвращает 3 наиболее повторяющихся элемента из входной строки. 
 
+from collections import Counter
 
 def top3(input_str):
-    pass
-    #TODO напишите Ваш код здесь
+
+    return [item[1] for item in Counter(input_str).most_common(3)]
 
 
 if __name__ == "__main__":
-    print(top3('11111111222222223333333344444444555555'))
+    print(top3('1111111122222222333333334444444455555555555555555'))
--- part2/top_3/solution.py
+++ part2/top_3/solution.py
@@ -4,3 +4,8 @@ from collections import Counter
 def top3(input_str):
     c = Counter(input_str)
     return [item[0] for item in c.most_common(3)]
+
+
+
+if __name__ == "__main__":
+    print(top3('11111111222222223333333344444444555555'))
